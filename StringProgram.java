064bc971d128d6b9f52a579651a46e2f73a53068
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-16 08:31:10 -0500

    Create StringProgram.java

---
+++ StringProgram.java
@@ -0,0 +1,8 @@
+public class StringProgram {
+  public static void main(String[] args) {
+    // Complete a method to take in a String object from Scanner, remove all * symbols,  
+    // and concatenate the four strings in reverse order. Print your new String.
+
+    
+  }
+}
