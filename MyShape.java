20d37485c49718b381b21b0dfd61eb37b519f6cb
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-16 08:19:13 -0500

    Create MyShape.java

---
+++ MyShape.java
@@ -0,0 +1,17 @@
+public class MyShape {
+  public static void main(String[] args) {
+    // declare variables for length, width, and area
+
+    // use a Scanner object to get length and width values
+
+    // call your methods to test your program
+
+    
+  }
+
+  // write your perimeter method here
+
+
+  // write your area method here
+  
+}
